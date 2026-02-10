0dd56788a96e1d8f536e67a17cd4c1913002ba3f
f0rmull <bwg.it616@gmail.com>
2026-02-10 17:01:13 +0300

    remove code

--- services/order-service/internal/pkg/connector/kafka/connector.go
+++ services/order-service/internal/pkg/connector/kafka/connector.go
@@ -8,22 +8,6 @@ import (
 	"github.com/IBM/sarama"
 )
 
-func MustConsumerGroup() sarama.ConsumerGroup {
-	cfg := sarama.NewConfig()
-
-	cfg.Consumer.Group.Rebalance.GroupStrategies = []sarama.BalanceStrategy{sarama.NewBalanceStrategyRoundRobin()}
-	cfg.Consumer.Offsets.AutoCommit.Enable = false
-	cfg.Consumer.Offsets.Initial = sarama.OffsetNewest
-
-	group, err := sarama.NewConsumerGroup(config.Instance().Kafka.Brokers, config.Instance().Kafka.ConsumerGroup, cfg)
-	if err != nil {
-		log.Fatalf(err.Error())
-		return nil
-	}
-
-	return group
-}
-
 func MustSyncProducer() sarama.SyncProducer {
 	saramaConfig := sarama.NewConfig()
 	saramaConfig.Producer.RequiredAcks = sarama.WaitForAll
